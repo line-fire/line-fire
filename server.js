7801382c613ab1fdb3d65225e08c2695df824dba
line-fire <5al09yfy@gmail.com>
2026-01-19 04:16:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const { Server } = require("socket.io");
 const PORT = process.env.PORT || 3000;
 const io = new Server(PORT, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+    cors: { origin: "*", methods: ["GET", "POST"] }
 });
-console.log(`--- SERVER START (Port: ${PORT}) ---`);
+console.log(`--- SERVER BAŞLATILDI ---`);
 io.on("connection", (socket) => {
-    socket.on("register", (machineName) => {
-        console.log(`[CONNECT] ${machineName} LOGIN DONE.`);
-    });
     socket.on("heartbeat", (data) => {
+        // Gelen veriye saat ekle
         const time = new Date().toLocaleTimeString("tr-TR", { timeZone: "Europe/Istanbul" });
-        console.log(`[PING - ${time}] ${data.machineName} -> CPU: %${data.cpuUsage}`);
-    });
-    socket.on("disconnect", () => {
-        console.log(`[LEAVE] SERVER DISCONNECT.`);
+        
+        const mesajPaketi = {
+            time: time,
+            machineName: data.machineName,
+            cpuUsage: data.cpuUsage
+        };
+
+        // Render'ın kendi loguna yaz
+        console.log(`[LOG] ${time} - ${data.machineName}`);
+
+        // ÖNEMLİ: Bu mesajı bağlı olan SANA (Monitor'e) gönder
+        io.emit("monitor_update", mesajPaketi);
     });
 });
+
 io.listen(PORT);
