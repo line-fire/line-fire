b2e1871f0df3f4337557ec228769a4ef13d410d0
line-fire <5al09yfy@gmail.com>
2026-01-19 04:14:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,22 @@
 const { Server } = require("socket.io");
-
-// Render'ın atadığı portu kullanır
 const PORT = process.env.PORT || 3000;
-
-// CORS ayarı: Her yerden gelen bağlantıyı kabul et
 const io = new Server(PORT, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
     }
 });
-
-console.log(`--- SERVER BAŞLATILDI (Port: ${PORT}) ---`);
-
+console.log(`--- SERVER START (Port: ${PORT}) ---`);
 io.on("connection", (socket) => {
-    // Client bağlandığında
     socket.on("register", (machineName) => {
-        console.log(`[BAĞLANDI] ${machineName} sisteme giriş yaptı.`);
+        console.log(`[CONNECT] ${machineName} LOGIN DONE.`);
     });
-
-    // Kalp atışı geldiğinde
     socket.on("heartbeat", (data) => {
-        // Türkiye saati ile zaman damgası
         const time = new Date().toLocaleTimeString("tr-TR", { timeZone: "Europe/Istanbul" });
         console.log(`[PING - ${time}] ${data.machineName} -> CPU: %${data.cpuUsage}`);
     });
-
-    // Bağlantı koptuğunda
     socket.on("disconnect", () => {
-        console.log(`[AYRILDI] Bir makine bağlantıyı kesti.`);
+        console.log(`[LEAVE] SERVER DISCONNECT.`);
     });
 });
-
-// Render'ın portu dinlemesi için sunucuyu aktif tutuyoruz (Socket.io standalone modunda bu yeterlidir)
 io.listen(PORT);
